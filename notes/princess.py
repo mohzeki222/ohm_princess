d3ec4dcba75f7072b054337b186587949df0c165
mohzeki222 <mohzeki@i.kyoto-u.ac.jp>
2019-05-05 22:59:57 +0900

    Add files via upload

--- notes/princess.py
+++ notes/princess.py
@@ -13,15 +13,39 @@ from tqdm import tqdm
 import os
 import PIL.Image as im        
 
+import urllib.error
 import urllib.request
-import sys
 
+def download_image(url, dst_path):
+    try:
+        data = urllib.request.urlopen(url).read()
+        with open(dst_path, mode="wb") as f:
+            f.write(data)
+    except urllib.error.URLError as error:
+        print(error)
+        
 def download_figs():
-
-    url = "https://github.com/mohzeki222/ohm_princess/blob/master/notes/princess.py"
-    title = princess2.py
-    urllib.request.urlretrieve(url,title)
     
+    url_list = []
+    for i in range(19):
+        url = "https://github.com/mohzeki222/ohm_princess/blob/master/notes/princess_fig/sira{:02}.jpg".format(i)
+        url_list.append(url)
+    download_dir = "/princess_fig"
+    for url in url_list:
+        filename = os.path.basename(url)
+        dst_path = os.path.join(download_dir, filename)
+        download_image(url, dst_path)
+        
+    url_list = []
+    for i in range(8):
+        url = "https://github.com/mohzeki222/ohm_princess/blob/master/notes/white_fig/sira{:02}.jpg".format(i)
+        url_list.append(url)
+    download_dir = "/white_fig"
+    for url in url_list:
+        filename = os.path.basename(url)
+        dst_path = os.path.join(download_dir, filename)
+        download_image(url, dst_path)
+
 class ResBlock(Chain):
     def __init__(self, ch, bn=True):
         layers = {}
